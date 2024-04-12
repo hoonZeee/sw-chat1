e00cfcb6577680d667579fdf1218d449074feb68
옥주헌 <1dhrwngjs@naver.com>
2024-04-12 23:48:47 +0900

    added fontschangeSelector

--- app.js
+++ app.js
@@ -1,48 +1,46 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var WebSocket = require('ws');
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+var WebSocket = require("ws");
 
 var app = http.createServer((req, res) => {
-    var _url = req.url;
-    var path = url.parse(_url, true).pathname;
+  var _url = req.url;
+  var path = url.parse(_url, true).pathname;
 
-    // 홈페이지 요청 처리
-    if (path === '/') {
-        fs.readFile(`./index.html`, (err, data) => {
-            if (err) {
-                res.writeHead(500);
-                res.end("Server Error");
-                return;
-            }
-            res.writeHead(200, { "Content-type": "text/html" });
-            res.end(data);
-        });
-    } else {
-        // 정적 파일 처리 (JavaScript, CSS 등)
-        fs.readFile(`.${path}`, (err, data) => {
-            if (err) {
-                res.writeHead(404);
-                res.end("Not Found");
-                return;
-            }
-            res.writeHead(200);
-            res.end(data);
-        });
-    }
+  if (path === "/") {
+    fs.readFile(`./index.html`, (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end("Server Error");
+        return;
+      }
+      res.writeHead(200, { "Content-type": "text/html" });
+      res.end(data);
+    });
+  } else {
+    fs.readFile(`.${path}`, (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.end("Not Found");
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
+  }
 });
 
 const wss = new WebSocket.Server({ server: app });
 
-wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(message) {
-        console.log('Received: %s', message);
-        ws.send(message); // 메시지 에코
-    });
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(message) {
+    console.log("Received: %s", message);
+    ws.send(message);
+  });
 
-    console.log('Client connected');
+  console.log("Client connected");
 });
 
 app.listen(3000, () => {
-    console.log(`Server running at http://localhost:3000/`);
+  console.log(`Server running at http://localhost:3000/`);
 });
